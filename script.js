85e7a05a340fdfb32c7c8be142a0099afcba1d95
Kajal Prajapati <kajalprajapati7451@gmail.com>
2025-08-16 10:22:20 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-  document.getElementById('current-year').textContent = new Date().getFullYear();
-        
-       
-        document.getElementById('contactForm').addEventListener('submit', async function(e) {
-            e.preventDefault();
-
-            const formData = new FormData(this);
-            const data = Object.fromEntries(formData.entries());
-
-            try {
-               
-                const res = await fetch('https://your-web-service-url.vercel.app/contact', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(data)
-                });
-
-                const result = await res.json();
-                document.getElementById('status').innerText = result.message;
-                
-                const successMsg = document.getElementById('form-success');
-                successMsg.style.display = 'block';
-                successMsg.classList.add('animate__animated', 'animate__fadeIn');
-                
-                this.reset();
-                
-                setTimeout(() => {
-                    successMsg.style.display = 'none';
-                }, 5000);
-            } catch (error) {
-                document.getElementById('status').innerText = 'Error submitting form. Please try again.';
-            }
-        });
+      var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    
+    navbarToggler.addEventListener('click', function() {
+        navbarCollapse.classList.toggle('show')
+    });
